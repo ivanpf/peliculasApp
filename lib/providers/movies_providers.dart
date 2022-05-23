80a5b574b3a3eb24809a45fde76269482709449a
Iván Pereyra <ivanpereyraf@protonmail.ch>
2022-05-23 00:30:15 -0300

    peticiones http y clase movie

--- lib/providers/movies_providers.dart
+++ lib/providers/movies_providers.dart
@@ -1,13 +1,24 @@
 // ignore_for_file: avoid_print, unnecessary_this
 
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:peliculas/models/models.dart';
 
 class MoviesProvider extends ChangeNotifier {
+  String _apiKey = 'a29872f47053ee73afabfd3dca4db851';
+  String _baseUrl = 'api.themoviedb.org';
+  String _language = 'es-ES';
+
   MoviesProvider() {
     print('MoviesProvider inicializando');
     this.getOnDisplayMovies();
   }
-  getOnDisplayMovies() {
-    print('getOnDisplayMovies');
+  getOnDisplayMovies() async {
+    var url = Uri.https(_baseUrl, '3/movie/now_playing',
+        {'api_key': _apiKey, 'language': _language, 'page': '1'});
+    final response = await http.get(url);
+    final nowPlayingResponse = NowPlayingResponse.fromJson(response.body);
+    print(nowPlayingResponse.results[1].title);
   }
 }
